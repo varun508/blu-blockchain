85c37e305a5106dd34b752d5d9b8544787923802
varun508 <imva508@gmail.com>
2019-07-08 05:56:03 +0530

    Create the demo blockchain

--- main.js
+++ main.js
@@ -14,3 +14,35 @@ class Block {
         return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
     }
 }
+
+class BlockChain {
+
+    // initializes the chain with the genesis block
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, { "name": "varun" }, '20/07/2019', '0');
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(block) {
+        block.previousHash = this.getLatestBlock().hash;
+        block.hash = block.calculateHash();
+        this.chain.push(block);
+    }
+}
+
+let coin = new BlockChain();
+
+for (let i = 0; i < 20; i++) {
+    var index = i + 1;
+    coin.addBlock(new Block(index, { name: 'user' + index }, (index) + '/07/2019'));
+}
+
+
+console.log(JSON.stringify(coin.chain,null,4));
